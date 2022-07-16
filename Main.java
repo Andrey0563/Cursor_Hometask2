53bea01ac255ff76e2abe7fad59d67176aff4478
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-16 17:16:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        int [] array = new int[13];
+
+        for (int i = 0; i < array.length; i++){
+            array [i] = rand.nextInt(100);
+
+        }
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+
+        }
+
+
+        }
+}
