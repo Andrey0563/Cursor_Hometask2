7b3176dc5b07665559580b5f806448683bf6c585
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-20 13:06:53 +0300

    Update seventhTask.java

--- seventhTask.java
+++ seventhTask.java
@@ -1,23 +1,53 @@
+import java.util.Arrays;
 import java.util.Random;
+//first var
+//
+//public class seventhTask {
+//    public static void main(String[] args) {
+//        Random random = new Random();
+//
+//        int [] arr = new int[10];
+//
+//        for (int i = 0; i<arr.length;i++){
+//            arr[i] = random.nextInt(50);
+//            if (arr[i] % 2 == 0){
+//                System.out.println(arr[i]);
+//            }
+//        }
+//
+//        int max = arr[0];
+//        for (int i = 0; i < arr.length; i++){
+//            if (arr[i]%2==0 && arr[i] > max) {
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println("The largest number is: "+max);
+//
+//    }
+//}
+////
+////
 
+//second var
 public class seventhTask {
     public static void main(String[] args) {
         Random random = new Random();
 
-        int [] arr = new int[7];
+        int [] arr = new int[10];
+
+        arr = new int[]{7, 22, -15, 4, 48, -35, -43, 23, 12, 22};
+
         int max = arr[0];
-
-        for (int i = 0; i<arr.length;i++){
-            arr[i] = random.nextInt(50);
-            System.out.println(arr[i]);
-
-        }
         for (int i = 0; i < arr.length; i++){
-            if (arr[i]>max && arr[i]%2==0){
-                max = arr[i];
+            if (arr[i]%2==0 ) {
+                if (arr[i] > max){
+                    max = arr[i];
+                }
+                System.out.println(arr[i]);
             }
-
         }
+        System.out.println(Arrays.toString(arr));
         System.out.println("The largest number is: "+max);
 
     }
