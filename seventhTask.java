eb8a315baa295abf5c5c4e89b38a153b351730ee
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-21 13:17:09 +0300

    Update seventhTask.java

--- seventhTask.java
+++ seventhTask.java
@@ -33,23 +33,24 @@ import java.util.Random;
 //second var
 public class seventhTask {
     public static void main(String[] args) {
-        Random random = new Random();
-
-        int [] arr = new int[10];
-
-        arr = new int[]{49, 22, -15, 4, 48, -35, -43, 23, 12, 22};
-
-        int max = 0;
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i]%2==0 ) {
-                if (arr[i] > max){
-                    max = arr[i];
+        int[] arr = new int[]{49, -22, 15, 4, 48, -35, 43, -23, -12, 22};
+        int i = 0;
+        boolean isEven = false;
+            int max = 0;
+            for (i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 0) {
+                    if (arr[i] > max) {
+                        max = arr[i];
+                        isEven = true;
+                        }
+                    }
                 }
-                System.out.println(arr[i]);
+            if (!isEven){
+                System.out.println("There is no one even number in array");
+                System.exit(0);
             }
-        }
         System.out.println(Arrays.toString(arr));
-        System.out.println("The largest number is: "+max);
+        System.out.println("The largest number is: " + max);
 
+        }
     }
-}
