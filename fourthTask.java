c3a35ab14e298cba521e20206dd0851f122e3305
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-23 23:24:41 +0300

    Update fourthTask.java

--- fourthTask.java
+++ fourthTask.java
@@ -3,22 +3,17 @@ import java.util.Random;
 public class fourthTask {
     public static void main(String[] args) {
         Random random = new Random();
-
-        int [] arr = new int[7];
+        int[] arr = new int[7];
         int max = arr[0];
-
-        for (int i = 0; i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(50);
             System.out.println(arr[i]);
-
         }
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i]>max){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
-
         }
-        System.out.println("The largest number is: "+max);
-
+        System.out.println("The largest number is: " + max);
     }
 }
