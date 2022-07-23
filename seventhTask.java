636dae2547e5f14f4c03191c20d6b0a3d634ba6b
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-23 23:26:37 +0300

    Update seventhTask.java

--- seventhTask.java
+++ seventhTask.java
@@ -6,24 +6,21 @@ import java.util.Random;
 //public class seventhTask {
 //    public static void main(String[] args) {
 //        Random random = new Random();
-//
-//        int [] arr = new int[10];
-//
-//        for (int i = 0; i<arr.length;i++){
+//        int[] arr = new int[10];
+//        for (int i = 0; i < arr.length; i++) {
 //            arr[i] = random.nextInt(50);
-//            if (arr[i] % 2 == 0){
+//            if (arr[i] % 2 == 0) {
 //                System.out.println(arr[i]);
 //            }
 //        }
-//
 //        int max = arr[0];
-//        for (int i = 0; i < arr.length; i++){
-//            if (arr[i]%2==0 && arr[i] > max) {
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] % 2 == 0 && arr[i] > max) {
 //                max = arr[i];
 //            }
 //        }
 //        System.out.println(Arrays.toString(arr));
-//        System.out.println("The largest number is: "+max);
+//        System.out.println("The largest number is: " + max);
 //
 //    }
 //}
@@ -34,23 +31,22 @@ import java.util.Random;
 public class seventhTask {
     public static void main(String[] args) {
         int[] arr = new int[]{-49, -22, -15, -4, -48, -35, -43, -23, -12, -22};
-        int i = 0;
         boolean isEven = false;
         int max = Integer.MIN_VALUE;
-             for (i = 0; i < arr.length; i++) {
-                if (arr[i] % 2 == 0 ) {
-                    if (arr[i] > max) {
-                        max = arr[i];
-                        isEven = true;
-                        }
-                    }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                    isEven = true;
                 }
-            if (!isEven){
-                System.out.println("There is no one even number in array");
-                System.exit(0);
             }
+        }
+        if (!isEven) {
+            System.out.println("There is no one even number in array");
+            System.exit(0);
+        }
         System.out.println(Arrays.toString(arr));
         System.out.println("The largest number is: " + max);
 
-        }
     }
+}
