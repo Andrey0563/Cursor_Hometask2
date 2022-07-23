8289cea2733d0346421a33ab6c7d14ffd1b6f9f0
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-23 23:25:28 +0300

    Update fifthTask.java

--- fifthTask.java
+++ fifthTask.java
@@ -3,22 +3,17 @@ import java.util.Random;
 public class fifthTask {
     public static void main(String[] args) {
         Random rand = new Random();
-
-        int [] arr = new int[15];
+        int[] arr = new int[15];
         int min = arr.length;
-
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(50);
             System.out.println(arr[i]);
         }
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i] < min){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
                 min = arr[i];
             }
         }
-        System.out.println("The lessees number is: "+min);
-
-
-
+        System.out.println("The lessees number is: " + min);
     }
 }
