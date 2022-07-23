e97a4d91eae68718e44483b55d7dd6d9d4d791f8
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-23 23:22:53 +0300

    Update thirdTask.java

--- thirdTask.java
+++ thirdTask.java
@@ -2,12 +2,12 @@ import java.util.Random;
 
 public class thirdTask {
     public static void main(String[] args) {
-        char[] options = {'F','Z','P','E','N','T','L','C','D','O'};
+        char[] options = {'F', 'Z', 'P', 'E', 'N', 'T', 'L', 'C', 'D', 'O'};
         char[] result = new char[10];
-        Random r=new Random();
+        Random r = new Random();
         System.out.println("The list of char symbols:");
-        for(int i=0;i<result.length;i++){
-            result[i]=options[r.nextInt(options.length)];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = options[r.nextInt(options.length)];
             System.out.println(result[i]);
         }
 
