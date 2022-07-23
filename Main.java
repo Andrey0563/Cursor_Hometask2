cd418580b09e35193a093dc015c632bd782edea6
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-23 23:21:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,14 @@ package com.company;
 
 import java.util.Arrays;
 import java.util.Random;
-
 public class Main {
-
     public static void main(String[] args) {
         int[] array = new int[20];
-        for (int i = 0; i < 20; i++) {
-            Random random = new Random();
-            int randomNumber = random.nextInt(101);
-            while (randomNumber % 2 != 0 || randomNumber == 0){
-                randomNumber = random.nextInt(101);
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            int randomNumber = random.nextInt(100);
+            while (randomNumber % 2 != 0 || randomNumber == 0) {
+                randomNumber = random.nextInt(100);
             }
             array[i] = randomNumber;
         }
