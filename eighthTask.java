ad0276150bad67ff772ee8e94a29872f8260048e
Andrey0563 <60926992+Andrey0563@users.noreply.github.com>
2022-07-23 23:27:20 +0300

    Update eighthTask.java

--- eighthTask.java
+++ eighthTask.java
@@ -1,10 +1,8 @@
 public class eighthTask {
     public static void main(String[] args) {
-        int [] initArr = {4,17,23,42,2,13,25,9,23};
-
-        for (int i = 0; i<initArr.length;i+=2){
+        int[] initArr = {4, 17, 23, 42, 2, 13, 25, 9, 23};
+        for (int i = 0; i < initArr.length; i += 2) {
             System.out.println(initArr[i]);
         }
-
     }
 }
